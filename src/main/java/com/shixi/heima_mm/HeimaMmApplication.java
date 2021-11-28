3ed16cb457b442d13b693bdacb23c4af095f0eb5
夕妄 <1040331024@qq.com>
2021-11-28 19:53:49 +0800

    zz-first-commit

---
+++ src/main/java/com/shixi/heima_mm/HeimaMmApplication.java
@@ -0,0 +1,13 @@
+package com.shixi.heima_mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimaMmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeimaMmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shixi/heima_mm/HeimaMmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shixi.heima_mm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimaMmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
